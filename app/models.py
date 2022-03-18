f5846a78472e80aa449fad9994231738bb4a840c
germandiaz17 <germanrodrigo.2003@htmail.com>
2022-03-17 22:29:32 -0500

    V2_modelos

--- app/models.py
+++ app/models.py
@@ -1,11 +1,29 @@
+from unicodedata import name
 from django.db import models
+from django.forms import CharField
 
 # Create your models here.
 
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    #def getName(self):
+    #   return self.name
+
+class Category(models.Model):
+    category = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.category
+
 class Book(models.Model):
     title = models.CharField(max_length=150)
-    author = models.CharField(max_length=100)
-    category = models.CharField(max_length=50)
+    author = models.ManyToManyField(Author)
+    category = models.ManyToManyField(Category)
     subject = models.CharField(max_length=100)
     publication_date = models.DateField()
 
